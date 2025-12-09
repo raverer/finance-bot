9c28c7fd7289c30490d7d5f4a0b86f88e41795c1
Rajib Rawal <rajibrawal.rr@gmail.com>
2025-12-10 00:35:18 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Chat from "./Chat";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
