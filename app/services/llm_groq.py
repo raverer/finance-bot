f88ed24cfbde03eb2697429f1320692537d00db8
Rajib Rawal <rajibrawal.rr@gmail.com>
2025-12-11 00:13:57 +0530

    Update llm_groq.py

--- app/services/llm_groq.py
+++ app/services/llm_groq.py
@@ -1,20 +1,18 @@
 import os
 from groq import Groq
 
-# Load API key
-GROQ_KEY = os.getenv("GROQ_API_KEY")
-client = Groq(api_key=GROQ_KEY)
+API_KEY = os.getenv("GROQ_API_KEY")
+client = Groq(api_key=API_KEY)
 
 def call_llm_groq(messages):
     try:
         completion = client.chat.completions.create(
             model="llama3-70b-8192",
             messages=messages,
-            temperature=0.2,
-            max_tokens=300
+            temperature=0.3,
         )
         return completion.choices[0].message["content"]
 
     except Exception as e:
-        print("Groq Error:", e)
+        print("GROQ ERROR:", e)
         return "I'm having trouble responding right now. Try again!"
